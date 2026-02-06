ef063081c450f722f573be6dd1c3d7c1f2568b04
Kay Markschies <kayschima@web.de>
2026-02-06 12:38:50 +0100

    Add user permissions to Inertia requests for enhanced authorization

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,7 +40,9 @@ class HandleInertiaRequests extends Middleware
             'name' => config('app.name'),
             'auth' => [
                 'user' => $request->user(),
+                'permissions' => $request->user()?->getPermissionsViaRoles()->pluck('name'),
             ],
+
             'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
         ];
     }
